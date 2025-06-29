0406bd2ab911a2ccf0707c593d0f139651a3b115
Ahmedik <isa732827@gmail.com>
2025-06-30 00:22:12 +0300

    changed the models and created a one-to-one relationship

--- db/models.py
+++ db/models.py
@@ -1,18 +1,24 @@
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
-from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+from sqlalchemy import String, DateTime, func, ForeignKey
 
 class Base(DeclarativeBase):
     pass
-
-class Commentss(Base):
-    __tablename__ = "comments"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    description: Mapped[str] = mapped_column(String(200))
 
 
 class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
-    comments: Mapped[str] = mapped_column(String(200))
     email: Mapped[str] = mapped_column()
+
+    comments: Mapped[list["Comment"]] = relationship(back_populates="user", cascade="all, delete")
+
+
+class Commentss(Base):
+    __tablename__ = "comments"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str] = mapped_column(String(200))
+    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), default=func.now())
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped[User] = relationship(back_populates="comments")
--- routers/users.py
+++ routers/users.py
@@ -33,3 +33,57 @@ async def comments(us: user.CreateUser, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(users)
     return users
+
+
+
+
+
+
+
+
+
+# @router.post("/", response_model=user.CreateUser)
+# async def comments(us: user.CreateUser, db: Session = Depends(get_db)):
+#     #email verification
+#     existing_user = db.query(User).filter(User.email == us.email).first()
+#     if existing_user:
+#         raise HTTPException(
+#             status_code=400,
+#             detail="Email already exists"
+#         )
+#
+#     users = User(name=us.name, comments=us.comments, email=us.email)
+#     db.add(users)
+#     db.commit()
+#     db.refresh(users)
+#     return users
+#
+# смотри вот моя ручка на fast api для добавления user как теперь будет работать у меня ручка и вообще как работает модели типа у меня user добавляет коментарий и так же может его смотреть?
+#
+#
+# from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+# from sqlalchemy import String, DateTime, func, ForeignKey
+#
+# class Base(DeclarativeBase):
+#     pass
+#
+#
+# class User(Base):
+#     __tablename__ = "users"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column()
+#     email: Mapped[str] = mapped_column()
+#
+#     comments: Mapped[list["Comment"]] = relationship(back_populates="user", cascade="all, delete")
+#
+#
+# class Commentss(Base):
+#     __tablename__ = "comments"
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     description: Mapped[str] = mapped_column(String(200))
+#     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), default=func.now())
+#
+#     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+#     user: Mapped[User] = relationship(back_populates="comments")
+#
+# А ВОТ модели
